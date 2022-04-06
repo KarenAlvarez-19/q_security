f649812ca4841b7f082a25c026c5d223e4e1f1c5
KarenAlvarez-19 <90125906+KarenAlvarez-19@users.noreply.github.com>
2022-04-06 17:27:04 -0500

    dialog

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -30,10 +30,31 @@
         </template>
 
         <template v-slot:after>
-          <q-btn round dense flat icon="send" />
+          <q-btn round dense flat icon="send" @click="dialog = true" />
         </template>
       </q-input>
   </q-footer>
+  <q-dialog v-model="dialog" persistent>
+      <q-card style="min-width: 350px">
+        <q-card-section>
+          <div class="text-h6">Upload file</div>
+        </q-card-section>
+
+        <q-file  bottom-slots v-model="model" label="File" counter class="q-pa-md">
+        <template v-slot:prepend>
+          <q-icon name="cloud_upload" @click.stop />
+        </template>
+        <template v-slot:append>
+          <q-icon name="close" @click.stop="model = null" class="cursor-pointer" />
+        </template>
+      </q-file>
+
+        <q-card-actions align="right" class="text-cyan">
+          <q-btn flat label="Cancel" v-close-popup />
+          <q-btn flat label="Send" v-close-popup />
+        </q-card-actions>
+      </q-card>
+    </q-dialog>
   </q-page>
 </template>
 
@@ -45,7 +66,8 @@ export default {
     return {
       text: ref(''),
       ph: ref(''),
-      dense: ref(false)
+      dense: ref(false),
+      dialog: ref(false),
     }
   }
 }
